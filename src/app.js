6a7ec89f8a9c4cd1e78eb28b2755409e67f50c94
¨Santiago <santiagogrela@gmail.com>
2023-09-03 20:44:29 -0300

    Service section

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const hola = document.getElementById('HOLA');
+
+const div = document.createElement('div');
+div.classList.add('card');
+div.innerHTML += `
+                    
+                `;
+hola.appendChild(div);
