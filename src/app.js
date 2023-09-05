16461b88fe6e44e6f0112d4f1e452ee753b75a8e
¨Santiago <santiagogrela@gmail.com>
2023-09-05 09:19:46 -0300

    Test deploy

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-const hola = document.getElementById('HOLA');
+const mostrarArticulos = document.getElementById('contenedorArticulos');
 
 const div = document.createElement('div');
-div.classList.add('card');
+div.classList.add('cards-containerArticulos');
 div.innerHTML += `
-                    
+                    <div class="cardsArticulos">
+                        <img class="imgCards" src="/public/images/articulo1.png">
+                        <h3 class="tituloCardsArticulos">Blanqueo de capitales – ¿conviene o no? cuestiones prácticas</h3>
+                        <p></p>
+                    </div>
                 `;
-hola.appendChild(div);
+mostrarArticulos.appendChild(div);
